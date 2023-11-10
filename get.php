d389f69782e956f58a9ecc9d2786a76ccaab8564
Eric Minel <66842416+198905@users.noreply.github.com>
2023-11-10 23:14:10 +0300

    Update get.php

--- get.php
+++ get.php
@@ -4,17 +4,14 @@ try {
     $bdd = new PDO('mysql:host=localhost;dbname=databasemessages;charset=utf8', 
     'root', '');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     } catch (PDOException $e) {
         echo 'Connection failed: ' . $e->getMessage();
     }
-
 $stmtGet='SELECT DISTINCT * FROM `messagestable` ORDER BY id ASC LIMIT 500000';
 $queryGet = $bdd->query($stmtGet);
 $messages = $queryGet->fetchAll();
-foreach ($messages as $msg) {?>
+foreach ($messages as $msg) { ?>
     
-        
 <?php if ($msg['pseudo']===$_SESSION['name']){ ?>
     <div class="messageFieldUser">
         <p id="nameSessionUser">
@@ -22,6 +19,7 @@ foreach ($messages as $msg) {?>
         </p>
         <p id="message">
             <?= $msg['msg'] ?>
+            
         </p>
     </div>
     <?php }else if ($msg['pseudo']!==$_SESSION['name']){ ?>
@@ -34,4 +32,3 @@ foreach ($messages as $msg) {?>
             </p>
         </div>
         <?php }}
-        
